2b315e9833bcde5b9e52e6e3ddf00f558e904d60
gmayweat <gmayweat@student.21-school.ru>
2021-10-25 14:39:53 +0300

    meow

---
+++ AutolayoutNKS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AutolayoutNKS
+//
+//  Created by Godrick Mayweather on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
